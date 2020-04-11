60709929cf6ee898101732957f044d3531a46cf8
Jonas Devries <jonas@good-it.be>
2020-04-11 13:28:19 +0200

    init spring security with boot

---
+++ src/main/java/be/jslm/darwin/DarwinApplication.java
@@ -0,0 +1,13 @@
+package be.jslm.darwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarwinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarwinApplication.class, args);
+    }
+
+}
